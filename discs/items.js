1f7c0c7be1a89528b82eefde03c69ed2c1f147ee
Bruno Pinho <bpinhosilva@gmail.com>
2018-03-21 01:36:47 -0300

    Set default disc items.

---
+++ discs/items.js
@@ -0,0 +1,40 @@
+let items = [
+    {
+        name: "Californication",
+        artist: "Red Hot Chili Peppers",
+        release_date: "June 8, 1999",
+        studio: "Cello Studios",
+        genre: "Alternative rock, funk rock",
+        label: "Warner Bros.",
+        producer: "Rick Rubin"
+    },
+    {
+        name: "Stadium Arcadium",
+        artist: "Red Hot Chili Peppers",
+        release_date: "May 9, 2006",
+        studio: "The Mansion",
+        genre: "Alternative rock, funk rock",
+        label: "Warner Bros.",
+        producer: "Rick Rubin"
+    },
+    {
+        name: "I'm with You",
+        artist: "Red Hot Chili Peppers",
+        release_date: "August 29, 2011",
+        studio: "",
+        genre: "Alternative rock, funk rock",
+        label: "Warner Bros.",
+        producer: "Rick Rubin"
+    },
+    {
+        name: "The Gateway",
+        artist: "Red Hot Chili Peppers",
+        release_date: "June 17, 2016",
+        studio: "The Sound Factory",
+        genre: "Alternative rock, funk rock",
+        label: "Warner Bros.",
+        producer: "Danger Mouse"
+    }
+];
+
+module.exports = items;
