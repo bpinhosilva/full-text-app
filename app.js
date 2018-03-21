e9277696dc6c8994f976edcad0a49f15cd43a4d9
Bruno Pinho <bpinhosilva@gmail.com>
2018-03-21 01:37:56 -0300

    Exposed dependencies from main app file.

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 'use strict';
 
 let express = require('express');
+let cors = require('cors');
 let favicon = require('serve-favicon');
 let cookieParser = require('cookie-parser');
 let bodyParser = require('body-parser');
+let config = require('config');
+let DBClient = require('./client/db_client');
+let DiscBusiness = require('./discs/business/disc_business');
 
 let app = express();
+
+let dbClient = new DBClient(config.get('dbConfig'));
+
+module.exports.getDBClient = () => {
+    return dbClient;
+};
+
+let discBusiness = new DiscBusiness();
+
+module.exports.getDiscBusiness = () => {
+    return discBusiness;
+};
+
+app.use(cors());
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -15,4 +33,6 @@ app.use(cookieParser());
 
 app.use('/', require('./routes'));
 
-module.exports = app;
+module.exports.getApp = () => {
+    return app;
+};
