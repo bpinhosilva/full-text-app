80de8c227ffdf4bb46eafdc7530f838867d8fc22
Bruno Pinho <bpinhosilva@gmail.com>
2018-03-21 01:36:34 -0300

    Exposed disc routes from main router.

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,6 @@
 let express = require('express');
 let router = express.Router();
 
-router.get('/', function (req, res, next) {
-    res.send("Ok");
-});
-
-router.get('/alive', (req, res, next) => {
-    res.send("alive");
-});
+router.use('/disc', require('../discs/routes'));
 
 module.exports = router;
