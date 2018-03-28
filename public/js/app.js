7a055d4ba7064424af55574de7366ed03c069eed
Bruno Pinho <bpinhosilva@gmail.com>
2018-03-28 02:05:14 -0300

    Created front-end main application file.

---
+++ public/js/app.js
@@ -0,0 +1,32 @@
+var app = angular.module("discApp", [
+    "ngRoute",
+    "ngResource",
+    "ngStorage"
+]);
+
+app.config(function ($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
+
+    $routeProvider
+        .when("/", {
+            redirectTo: "/disc"
+        })
+        .when("/disc", {
+            templateUrl: "views/disc/disc_view.html",
+            controller: "DiscCtrl",
+            controllerAs: "discCtrl"
+        })
+        .when("/disc/new", {
+            templateUrl: "views/disc/disc_new_view.html",
+            controller: "DiscNewCtrl",
+            controllerAs: "discNewCtrl"
+        })
+        .when("/disc/:id/edit", {
+            templateUrl: "views/disc/disc_edit_view.html",
+            controller: "DiscEditCtrl",
+            controllerAs: "discEditCtrl"
+        })
+        .otherwise({
+            redirectTo: "/disc"
+        });
+});
