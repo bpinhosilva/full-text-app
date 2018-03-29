1b5365bc2b6298d5cdf91cef816c52a5b69f3c38
Bruno Pinho <bpinhosilva@gmail.com>
2018-03-29 02:07:34 -0300

    Set search config to development.js file.

--- config/development.js
+++ config/development.js
@@ -10,20 +10,19 @@ module.exports = {
         connectionLimit: 10
     },
     searchConfig: {
-        disc: {
-            index: 'mediacollection',
-            type: 'disc',
-            port: 9200,
-            host: process.env.ES_HOST || 'localhost',
-            schema: {
-                /*_id>
+        index: 'mediacollection',
+        type: 'disc',
+        port: 9200,
+        host: process.env.ES_HOST || 'localhost',
+        schema: {
+            disc: {
                 name: {type: 'text'},
                 artist: {type: 'keyword'},
                 release_date: {type: 'text'},
                 studio: {type: 'text'},
                 genre: {type: 'text'},
                 label: {type: 'text'},
-                producer: {type: 'text'}*/
+                producer: {type: 'text'}
             }
         }
     }
